421eed67a80f5e784ee5278d3eedd9f0be63d3bd
jrw <197988939+Jrwick@users.noreply.github.com>
2026-01-19 22:37:36 +0000

    Add function for computer to make a choice

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
+console.log(getComputerChoice());
+
+function getComputerChoice() {
+  let number = Math.floor(Math.random() * 3);
+
+  switch (number) {
+    case 0:
+      return "Rock";
+    case 1:
+      return "Paper";
+    case 2:
+      return "Scissors";
+  }
+}
 
